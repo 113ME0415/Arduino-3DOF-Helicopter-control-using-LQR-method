3f16a0e4527f079783f37f3809063481cf58e64c
Azith <31697333+Veeraboinaa1@users.noreply.github.com>
2017-10-17 23:29:02 -0400

    Add files via upload

---
+++ LQR_Final_Code.ino
@@ -0,0 +1,223 @@
+//LQR control method for regulating the Quansers 3DOF Helicopter
+//In this method the control theory is valid at equilibrium point only.
+// Main objective is to control or regulate the angular position to zero i.e equilibrium point.
+// Motors max voltage is +/-24V and continous or rated voltage +/-12V.
+// The velocities are estimated by Discrete High Pass Filter.
+// counterweight is positioned at 2nd slot.
+
+#include <SPI.h>
+#include "Wire.h"
+
+//Sampling time
+unsigned int T = 10 ;
+unsigned long currentMillis1 =0;
+long previousMillis = 0;
+long previousMillis1 = 0;
+long previousMillis2 = 0;
+long previousMillis3 = 0;
+
+
+// variables
+float val1    = 0;   // analog value 
+float val2    = 0;   // analog value 
+float val3    = 0;   // analog value 
+float oldVal1 = 0;
+float oldVal2 = 0;
+float oldVal3 = 0;
+
+
+//Motor PWM output Pins
+ int PWM1 = 5;         
+ int PWM2 = 6;
+ int PWM_Vf,PWM_Vb;
+ // Encoder resolution
+ int Enc_res1= 1024;//Enc_res2= 8192;
+ // Motor Direction Pins
+ int Dir_pin1 = 44;
+ int Dir_pin2 = 46;
+ unsigned long dt1=0;
+ float val11=0;
+ 
+// Encoder pins
+const int slaveSelectEnc1 = 53;   // Slave Select pins for Elevation Encoder
+const int slaveSelectEnc2 = 49;   // Slave Select pins for Pitch Encoder
+const int slaveSelectEnc3 = 48; // Slave Select pins for Travel Encoder
+
+// These hold the current encoder count.
+signed long enc1count = 0;
+signed long enc2count = 0;
+signed long enc3count = 0;
+
+// states of the system
+float x1=0;//= 0.479;
+float y1=0;
+float z1=0;
+float x2=0;
+float y2=0;
+float z2=0;
+
+
+const float pi = 3.14159267;
+
+// Feedback gains or LQR gains
+const float k11= 4.472;                           
+const float k12= 35.478;
+const float k13= -12.247;
+const float k14= 7.234;
+const float k15= 7.8314;
+const float k16= -11.442;
+const float k21= k11;
+const float k22= -k12 ;
+const float k23= -k13 ;
+const float k24= k14;
+const float k25= -k15;
+const float k26= -k16 ;
+float U1,U2;
+float Vf,Vb ;
+
+// defined variable which are used for discrete High-pass Filter 
+float E1=0;
+float E2=0;
+float E3=0;
+float F1=0;
+float F2=0;
+float F3=0;
+int i =0;
+
+
+// Void setup
+void setup() 
+{
+  // put your setup code here, to run once:
+   Wire.begin(); 
+   Serial.begin(57600);
+   pinMode(PWM1 , OUTPUT);
+   pinMode(PWM2 , OUTPUT);
+   pinMode(Dir_pin1, OUTPUT);
+   pinMode(Dir_pin2, OUTPUT);
+   pinMode(28, OUTPUT);
+   pinMode(30, OUTPUT);
+   pinMode(32, OUTPUT);
+   pinMode(34, OUTPUT);
+   
+   initEncoders();       
+   Serial.println("Encoders Initialized...");  
+   clearEncoderCount();  
+   Serial.println("Encoders Cleared...");
+}
+
+void loop() 
+{
+    unsigned long currentMillis = millis();  
+    float Time1 = float(currentMillis - previousMillis);
+   unsigned long Time = currentMillis - previousMillis; 
+    if( Time > T)
+    {
+      
+    previousMillis = currentMillis; 
+
+    // Calulating the position of elevation x1, pitch y1 and travel z1.
+    x1 = readValue1();
+    y1 = readValue2();
+    z1 = readValue3();
+    
+    
+    // Saturating the angular positon or providing the limits.
+    
+    if (x1 > 0.78)
+    {
+      x1 = 0.78;
+    }
+    else if (x1<-0.78)
+    {
+      x1 = -0.78;
+    }
+    if (y1 > 0.78)
+    {
+      y1 = 0.78;
+    }
+    else if (y1<-0.78)
+    {
+      y1 = -0.78;
+    }
+    if (z1 > 0.78)
+    {
+      z1 = 0.78;
+    }
+    else if (z1<-0.78)
+    {
+      z1 = -0.78;
+    }
+   
+    //Serial.println("First Angular position");
+    //Serial.print(" x1: "); 
+    Serial.print(x1);  Serial.print(" ");
+    //Serial.print(" y1: "); 
+    Serial.print(y1);  Serial.print(" ");
+    //Serial.print(" z1: "); 
+    Serial.print(z1); Serial.print(" ");
+
+   // angular velocities of all three degree of freedom
+     
+   x2 = Cal_Elev_vel(x1);
+   y2 = Cal_Pitch_vel(y1);
+   z2 = Cal_Travel_vel(z1);
+
+  
+   // Serial.println("First Angular velocities");
+   // Serial.print(" x2: "); 
+    Serial.print(x2); Serial.print(" ");                 
+   //  Serial.print(" y2: "); 
+    Serial.print(y2);  Serial.print(" ");
+   //  Serial.print(" z2: "); 
+    Serial.print(z2);  Serial.print(" ");
+
+    
+    // feedback control signal calculated from LQR method 
+    // where 7.5V is bias control voltage.
+    
+    U1 = -(x1*k11+y1*k12+z1*k13+x2*k14+y2*k15+z2*k16)+7.5;
+    U2 = -(x1*k21+y1*k22+z1*k23+x2*k24+y2*k25+z2*k26)+7.5;
+      
+    //Serial.print("Controller1: "); 
+    Serial.print(U1); Serial.print(" ");      // Control signal for Front motor
+   // Serial.print("Controller2: "); 
+    Serial.println(U2);                       // Control signal for Back motor
+
+
+    // We need to mention the voltage limits.
+
+    float lim =24.00;
+    if (U1 > lim)
+    {
+      U1 = lim;
+    }
+    else if (U1 < -lim)
+    {
+      U1 = -lim;
+    }
+    if (U2 > lim)
+    {
+      U2 = lim;
+    }
+    else if (U2 < -lim)
+    {
+      U2 = -lim;
+    }
+    
+    PWM_Vf = map(U1,0,24,0,255);
+    analogWrite(PWM1,PWM_Vf);         // analog output which is given to front motor  
+    digitalWrite(Dir_pin1,LOW);       // digital pin for defining the direction of rotation of the motor in this case propeller or blades rotate in anti-clockwise 
+    PWM_Vb = map(U2,0,24,0,255);      // define the PWM signal to volage range. 
+    analogWrite(PWM2,PWM_Vb);         // analog output which is given to back motor
+    digitalWrite(Dir_pin2,LOW);      
+   
+    
+    i = i+1;  // just to check no. of loops
+
+    unsigned long currentMillis10 = millis();   // Gives current time in ms
+    unsigned long TS= currentMillis10- currentMillis; 
+  //  Serial.println(TS);   // time in milli seconds for one complete loop
+
+ }
+}
---
+++ core_functions.ino
@@ -0,0 +1,50 @@
+// function for reading Elevation angular position
+
+float readValue1()
+{
+  float u;
+  currentMillis1 = millis();
+  enc1count = 131+readEncoder(1);   // intial condition is 131 or 0.2 radians. 
+  u = ((-enc1count*360)/(4*1024))*pi/180; //elevation  // encoder resolution is 4086
+  val1 = u;
+  return val1;
+  previousMillis1 = currentMillis1;
+  oldVal1 = val1;
+}
+
+
+
+//----------------------------------------------------------------------
+// function for reading Pitch angular position
+
+float readValue2()
+{
+  float u;
+  unsigned long currentMillis3 = millis();
+  enc2count = readEncoder(2);                           // intial condition is 0 radians.
+  u = ((enc2count*360)/(4*1024))*pi/180; //Pitch // encoder resolution is 4086
+  val2 = u;
+  return val2;
+  previousMillis2 = currentMillis3;
+  oldVal2 = val2;
+}
+
+
+//-----------------------------------------------------------------------
+// function for reading Travel angular position
+
+float readValue3()
+{
+  float u;
+  unsigned long currentMillis5 = millis();
+  enc3count = readEncoder(3);               // intial condition is 0 radians.
+  u = ((enc3count*360)/(8*1024))*pi/180; // encoder resolution is 8192
+  val3 = u;
+  return val3;
+  previousMillis3 = currentMillis5;
+  oldVal3 = val3;
+}
+
+
+
+
